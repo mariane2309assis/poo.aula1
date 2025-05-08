5d3d20c0d24d744d1f740976f83ca9ee33fb8e71
mariane <mariane.assis.bezerra@gmail.com>
2025-05-08 16:49:25 -0300

    conta bancária

--- biblioteca.py
+++ biblioteca.py
@@ -6,6 +6,7 @@ class Pessoa():
             self.falando= False
             self.comendo= False
             self.dormindo= False
+
         def pararComer(self):
             if self.comendo == False:
                 print(f"{self.nome}, não está comendo")
@@ -53,3 +54,56 @@ class Pessoa():
             else:
                 print(f"{self.nome} esta falando")
                 self.comendo = True
+
+class ContaBancaria():
+    def __init__(self, numero, nome, tipo, saldo, limite):
+        self.numero=numero
+        self.saldo=0
+        self.nome=nome
+        self.tipo=tipo
+        self.status=False
+        self.limite= 0
+
+
+    def ativarconta (self):
+        if self.status == False:
+            print("conta ativa c sucesso")
+            self.status = True
+        else:
+            print("conta já está ativa")
+
+    def statussDsativar(self):
+        if self.status == False:
+            print("conta desativada")
+        else:
+            print("desaativa")
+
+    def desativarconta(self):
+        if self.status == True:
+            print("conta desativada c sucesso")
+            self.status = False
+        else:
+            print("conta está desativada")
+
+    def  verificarsaldo(self):
+
+        print(f"o saldo é :{self.saldo}")
+
+    def depositar(self, valor):
+        self.saldo+=valor
+        print(f"ficou {self.saldo}")
+
+    def sacar(self,valor):
+        if self.status == False:
+            print("conta inativa,")
+        else:
+            if (self.saldo + self.limite) > valor:
+                print("saldo negtivo, não pode sacar")
+            else:
+                self.saldo -= valor
+                print("saque com sucesso")
+
+
+    def ajustarlimite(self,limite):
+        self.ajustarlimite = limite
+        print(f"limite adicionado")
--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,6 @@
 from biblioteca import Pessoa
 
-aluno01= Pessoa (78, "Mari", 20 )
+'''aluno01= Pessoa (78, "Mari", 20 )
 print(aluno01.peso)
 print(aluno01.nome)
 print(aluno01.idade)
@@ -9,3 +9,16 @@ print()
 aluno01.comer("cachorro-quente")
 aluno01.falar()
 aluno01.dormir()
+'''
+
+from biblioteca import ContaBancaria
+pessoa1 = ContaBancaria (2, "maria", "corrente", 400,500)
+
+pessoa1.ativarconta()
+pessoa1.depositar(1000)
+pessoa1.ajustarlimite(1000)
+pessoa1.sacar(1500)
+pessoa1.verificarsaldo()
+pessoa1.depositar(200)
+pessoa1.verificarsaldo()
+
